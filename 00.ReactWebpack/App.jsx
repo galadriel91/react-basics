993d5feb5cd0a3264db29d463418c9249f053413
galadriel91 <g_ala@Naver.com>
2022-06-13 11:51:23 +0900

    Add: Components - ReactWebpack

--- 00.ReactWebpack/App.jsx
+++ 00.ReactWebpack/App.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import 'Style/index'
+import ReactWebpack from 'Components/ReactWebpack'
 
 export const App = () => {
     return (
         <div className='container'>
-            App
+            <ReactWebpack/>
         </div>
     )
 }
