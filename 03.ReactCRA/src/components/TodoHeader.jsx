bb2924be733f41db1914a362c70bb554fe6046ee
GalaDriel <g_ala@naver.com>
2022-12-07 20:47:04 +0900

    Add: Components - TodoHeader

---
+++ 03.ReactCRA/src/components/TodoHeader.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import '../assets/scss/components/TodoHeader.scss'
+
+const TodoHeader = () => {
+    return ( 
+        <header>
+            <h1>Todo Header</h1>
+        </header>
+    );
+}
+
+export default TodoHeader;
