4f712dbb05def516b31d904b6c6e810887b532dd
GalaDriel <g_ala@naver.com>
2022-12-07 20:41:17 +0900

    Add: StyleSheets

---
+++ 03.ReactCRA/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import './assets/scss/index.scss'
+
+const App = () => {
+  return ( 
+    <div className='container'>
+      App
+    </div>
+  );
+}
+export default App;
