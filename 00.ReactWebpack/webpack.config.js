6750433b6daeac59942fe05bff29cddc5e16746c
GalaDriel <g_ala@naver.com>
2022-12-07 16:03:54 +0900

    <React Webpack Initial Commit>

---
+++ 00.ReactWebpack/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+
+module.exports = {
+    mode:'development',
+    devtool:'eval',
+    resolve:{
+        extensions:['.scss' , '.module.scss' , '.css' , '.js' , '.jsx'],
+        alias:{
+            Style:path.resolve('./src/assets/scss/'),
+            Image:path.resolve('./src/assets/image/'),
+            Components:path.resolve('./src/components/'),
+            Store:path.resolve('./src/store/'),
+        }
+    },
+    entry:{
+        app:'./main.js'
+    },
+    module:{
+        rules:[
+            {
+                test:/\.jsx?/,
+                loader:'babel-loader',
+                options:{
+                    presets:['@babel/preset-env' , '@babel/preset-react']
+                }
+            },
+            {
+                test:/\.css$/,
+                use:['style-loader' , 'css-loader']
+            },
+            {
+                test:/\.scss$/,
+                use:['style-loader' , 'css-loader' , 'sass-loader']
+            },
+            {
+                test: /\.(png|jpe?g|gif)$/i,
+                use: [
+                    {
+                        loader: 'file-loader',
+                        options: {
+                            name: '[name].[contenthash].[ext]',
+                            outputPath: 'static/img',
+                            esModule: false
+                        }
+                    }
+                ],
+            }
+        ]
+    },
+    plugins:[
+
+    ],
+    output:{    
+        filename:'app.js',
+        path:path.join(__dirname , 'dist'),
+        publicPath:'/dist'
+    },
+    devServer:{
+        devMiddleware:{publicPath:'/dist'},
+        static:{directory:path.resolve(__dirname)},
+        hot:true
+    }
+}
