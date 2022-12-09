93b1851458750de9656efa828f47f9757ca32d84
GalaDriel <g_ala@naver.com>
2022-12-09 15:08:02 +0900

    Update: Define typescript with redux

--- 05.ReactType/src/components/TodoHeader.tsx
+++ 05.ReactType/src/components/TodoHeader.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-// import { useSelector } from 'react-redux'
+import { useAppSelector } from '../store/hooks'
 import '../assets/scss/components/TodoHeader.scss'
 
 const TodoHeader = () => {
-    // const title = useSelector(state => state.todo.title)
+    const title = useAppSelector(state => state.todo.title)
     return ( 
         <header>
-            <h1>React Todo</h1>
+            <h1>{title}</h1>
         </header>
     );
 }
