d803764fda73800860a8caba54c3a11555d09c1c
GalaDriel <g_ala@naver.com>
2022-12-09 15:26:39 +0900

    Add: Components - TodoInput

--- 05.ReactType/src/App.tsx
+++ 05.ReactType/src/App.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './assets/scss/index.scss'
 import TodoHeader from './components/TodoHeader';
+import TodoInput from './components/TodoInput';
 
 const App = () => {
   return ( 
     <div className='container'>
       <TodoHeader/>
+      <TodoInput/>
     </div>
   );
 }
---
+++ 05.ReactType/src/components/TodoInput.tsx
@@ -0,0 +1,40 @@
+import React, { useState , useCallback , useRef , useEffect } from 'react'
+// import { ADD_TODO } from '../store/features/todo'
+import Style from '../assets/scss/components/TodoInput.module.scss'
+
+const TodoInput = () => {
+    const [value , setValue] = useState('')
+    const input = useRef('')
+    
+    useEffect(()=>{
+        // input.current.focus()
+    },[])
+
+    const onChangeInput = useCallback(()=>{
+        // setValue(e.target.value)
+    },[])
+
+    const onSubmitForm = useCallback(()=>{
+        // e.preventDefault()
+        // if(value.length){
+        //     dispatch(ADD_TODO({
+        //         id:Date.now(),
+        //         isCheck:false,
+        //         content:value,
+        //     }))
+        //     setValue('')
+        //     input.current.focus()
+        // }else{
+        //     alert('다시 한번 확인 부탁 드립니다.')
+        // }
+    },[])
+
+    return (
+        <form className={Style.inputWrap} onSubmit={onSubmitForm}>
+            <input type="text" placeholder="오늘 해야할 일을 입력해 주세요" value={value} onChange={onChangeInput}/>
+            <button className="xi-arrow-right" type="submit"></button>
+        </form>
+    )
+}
+
+export default TodoInput
